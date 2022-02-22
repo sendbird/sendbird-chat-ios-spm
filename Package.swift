fcd85e3c6a75c37a816d0fdc1fd68ad546107ce4
Jed Gyeong <jed@sendbird.com>
2022-02-22 15:02:01 +0900

    * Fixed issue with channel sync where channels created in the background did not properly load. * Deprecated `SBDAuthenticateDelegate` and `SBDConnectionManager`.

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SendBirdSDK",
-            url: "https://github.com/sendbird/sendbird-ios-framework/releases/download/v3.1.3/SendBirdSDK.xcframework.zip",
-            checksum: "1ef364824b85bc94ae542fe4e3da883847e43876f2a98e95d5a7e58e74c4e4b2"
+            url: "https://github.com/sendbird/sendbird-ios-framework/releases/download/v3.1.4/SendBirdSDK.xcframework.zip",
+            checksum: "1871712ad7738a580268c8bde2027901a1d40253a819e448b7036abc92de8255"
         ),
     ]
 )
