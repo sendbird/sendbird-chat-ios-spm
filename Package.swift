74fc22ee6e50349c861ad1af77d4e49b0dd52839
Pitt Cho <pitt.cho@sendbird.com>
2022-09-28 19:26:27 +0900

    * Implemented synchronous initialization for SBDMain * Improved stability when sending FILE messages * Removed older implementation of polls from v3

--- Package.swift
+++ Package.swift
@@ -16,8 +16,9 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SendBirdSDK",
-            url: "https://github.com/sendbird/sendbird-ios-framework/releases/download/v3.1.27/SendBirdSDK.xcframework.zip",
-            checksum: "08e1cb8814b00a056486e1c83eb61bd3ce65909bf11ef736224d7a9c16a859bc"
+            url:
+                "https://github.com/sendbird/sendbird-ios-framework/releases/download/v3.1.28/SendBirdSDK.xcframework.zip",
+            checksum: "859a694c58007ae4576afa6d80b9050bb699651da660dc38575feff24c375ed3"
         ),
     ]
 )
