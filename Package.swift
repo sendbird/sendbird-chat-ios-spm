f56f32aba99ea373e1b83fa084ba089efbdd7b16
Ernest Hong <hsh3592@gmail.com>
2022-10-25 00:08:13 +0900

    Release - 3.1.31

--- Package.swift
+++ Package.swift
@@ -1,8 +1,7 @@
+
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
-
 let package = Package(
     name: "SendBirdSDK",
     platforms: [.iOS(.v10)],
@@ -16,9 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SendBirdSDK",
-            url:
-                "https://github.com/sendbird/sendbird-ios-framework/releases/download/v3.1.29/SendBirdSDK.xcframework.zip",
-            checksum: "26a8a2e293a05704b582c53eb235fb977b6c1bc6867af07f7c7819ee42ccbdbf"
+            url: "https://github.com/sendbird/sendbird-ios-framework/releases/download/v3.1.31/SendBirdSDK.xcframework.zip",
+            checksum: "ab7626017c86670e5ab3b1f027adf39bc2f17f4b0d26406f88e4c7d26966c4e8"
         ),
     ]
 )
