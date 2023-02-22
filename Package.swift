bef90310423f7c0182801068649aa490bce51d04
Distiller <sha.sdk_deployment@sendbird.com>
2023-02-22 09:25:47 +0000

    Release - 3.1.42

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SendBirdSDK",
-            url: "https://github.com/sendbird/sendbird-ios-framework/releases/download/v3.1.41/SendBirdSDK.xcframework.zip",
-            checksum: "ddbd15df08682d8c4d1cafa3cfdc4305933d93db00cae7bb0711915c3128f8f6"
+            url: "https://github.com/sendbird/sendbird-ios-framework/releases/download/v3.1.42/SendBirdSDK.xcframework.zip",
+            checksum: "26d91021d0fe597d365713e9d49b9ebbf461792b88f66fce50e75551dfee3c72"
         ),
     ]
 )
