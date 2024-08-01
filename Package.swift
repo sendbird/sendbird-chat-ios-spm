44d22605627add7476e7d6723f076a2a07440d30
Minhyuk Kim <kimminhyuk1004@gmail.com>
2024-08-01 11:32:12 +0900

    Release v3.1.54

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SendBirdSDK",
-            url: "https://github.com/sendbird/sendbird-ios-framework/releases/download/v3.1.53/SendBirdSDK.xcframework.zip",
-            checksum: "097017e144ea54766744274f51f0d059168c1e6488de9bd9f1aef46c567d13f3"
+            url: "https://github.com/sendbird/sendbird-ios-framework/releases/download/v3.1.54/SendBirdSDK.xcframework.zip",
+            checksum: "32c22c667a55fc6a5f0032b04711169cfdccddd9c6e6ecd3650c388cb4c74d99"
         ),
     ]
 )
