31d7c7b4c592e9e78569219b1935fb92789a98b5
Minhyuk Kim <kimminhyuk1004@gmail.com>
2024-08-01 11:33:14 +0900

    Release v3.1.56

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 let package = Package(
     name: "SendBirdSDK",
-    platforms: [.iOS(.v10)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(
             name: "SendBirdSDK",
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SendBirdSDK",
-            url: "https://github.com/sendbird/sendbird-ios-framework/releases/download/v3.1.55/SendBirdSDK.xcframework.zip",
-            checksum: "5a4c7b30f4fb4624e79dab739b29d9b961b8a085e3c20647d95dc0c978e533f7"
+            url: "https://github.com/sendbird/sendbird-ios-framework/releases/download/v3.1.56/SendBirdSDK.xcframework.zip",
+            checksum: "1fbf2a1f479f5cf1944462eb51ae7baf6b82d006662b34015fa80502befa3f30"
         ),
     ]
 )
